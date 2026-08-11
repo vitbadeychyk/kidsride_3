e92af2795f182572455c51da1880a4483e6d201b
vitbadeychyk <v.kushnir03@gmail.com>
2026-08-11 17:18:49 +0300

    Update telegram-test.js

--- api/telegram-test.js
+++ api/telegram-test.js
@@ -1,5 +1,4 @@
 import { sendTelegram } from './order-notification-lib.js';
-
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
