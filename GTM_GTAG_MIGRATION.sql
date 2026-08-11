4d93d0c94e647b4ff2281d61cb22e8809db84917
vitbadeychyk <v.kushnir03@gmail.com>
2026-08-11 17:41:08 +0300

    Replace project files

--- GTM_GTAG_MIGRATION.sql
+++
@@ -1,7 +0,0 @@
--- Міграція: додаємо gtm_id до таблиці settings_seo
--- Виконати один раз у Supabase SQL Editor
-
-ALTER TABLE settings_seo ADD COLUMN IF NOT EXISTS gtm_id text DEFAULT '';
-
--- Перевірка:
--- SELECT id, gtm_id, ga_id, fb_pixel FROM settings_seo WHERE id = 1;
